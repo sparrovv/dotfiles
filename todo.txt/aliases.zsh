64866303a9808bec80478764c5c9667db05101dd
Micha Wrobel <sparrovv@gmail.com>
2012-01-29 16:30:38 +0100

    Fix paths to todo.txt

--- todo.txt/aliases.zsh
+++ todo.txt/aliases.zsh
@@ -1,9 +1,9 @@
 # todo.sh: https://github.com/ginatrapani/todo.txt-cli
 function t() { 
   if [ $# -eq 0 ]; then
-    todo.sh ls
+    /Users/michalwrobel/todos/todo.sh ls
   else
-    todo.sh $*
+    /Users/michalwrobel/todos/todo.sh $*
   fi
 }
 
