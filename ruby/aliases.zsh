b4261321fbbca619d80d96c88e7f4ae56895e3b0
Michal Wrobel <sparrovv@gmail.com>
2012-06-05 11:30:14 +0200

    New alias re - bundle exec

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -7,6 +7,8 @@ alias rs='rails server'
 alias rc='rails console'
 alias r='bundle exec rake'
 alias s='bundle exec rspec'
+alias re='bundle exec'
+
 
 alias fman='bundle exec foreman start'
 alias rlog='tail -f log/development.log'
