20e60f8b1fa61146642ba1e1324abbceae5f06b6
Michal Wrobel <sparrovv@gmail.com>
2013-02-13 08:50:45 +0000

    Fix git push alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,7 +11,7 @@
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push -u origin HEAD'
 alias gd='git diff'
 alias gds='git diff --cached'
 alias gc='git commit'
