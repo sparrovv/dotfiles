ab47f2fd07221728beaede7625caf22b6502d0c5
Michal Wrobel <sparrovv@gmail.com>
2014-11-20 23:14:16 +0000

    Get rbenv path before /url/local/bin

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
 export GOPATH=/Users/mwrobel/workspace/gocode
-export PATH=".:bin:/opt/local/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$GOPATH/bin:$PATH"
+export PATH=".:$HOME/.rbenv/shims:bin:/opt/local/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$GOPATH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
