6b79185b9dd178a300625358e2baf84e22f4c09a
Michal Wrobel <sparrovv@gmail.com>
2014-12-05 21:07:47 +0000

    Add GOPATH to CDPATH

---
+++ go/env.zsh
@@ -0,0 +1,5 @@
+export GOPATH=/Users/mwrobel/workspace/gocode
+# path to godoc and to the my go bin
+export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin:$PATH
+
+export CDPATH="$CDPATH:$GOPATH/src/github.com/"
--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,3 @@
-export GOPATH=/Users/mwrobel/workspace/gocode
-
 export PATH=".:$HOME/.rbenv/shims:bin:/opt/local/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$HOME/.sfs:$ZSH/bin:$GOPATH/bin:$PATH"
-# godoc
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
